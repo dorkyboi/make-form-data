1b94862663b6d5d7350551f0d3e693e780065768
Dmytro Kardava <dima.kardava@gmail.com>
2021-07-30 20:05:12 +0200

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+const recursiveAppend = (formData, field, value) => {
+    if (value instanceof File) {
+        formData.append(field, value, value.name);
+        return formData;
+    }
+
+    if (Array.isArray(value)) {
+        value.forEach((val, index) => recursiveAppend(formData, field + '[' + index + ']', val));
+        return formData;
+    }
+
+    if (value === null) {
+        formData.append(field, '');
+        return formData;
+    }
+
+    if (value && typeof value === 'object') {
+        Object.keys(value).forEach(key => recursiveAppend(formData, field ? `${field}[${key}]` : key, value[key]));
+        return formData;
+    }
+
+    if (typeof value === 'boolean') {
+        formData.append(field, !!value ? '1' : '');
+        return formData;
+    }
+    return formData.append(field, value);
+};
+
+export default function makeFormData(data) {
+    if (typeof data !== 'object')
+        throw new TypeError(`Invalid argument of type [${typeof data}] passed to makeFormData. Only objects are allowed.`);
+    return recursiveAppend(new FormData(), null, data);
+};
